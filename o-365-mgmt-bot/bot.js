4fdd2f0311182e22383a214cc0d5c9c11a571061
Rafael Nunes <rafael.fn@outlook.com>
2017-10-20 11:48:42 +0200

    1st commit

---
+++ o-365-mgmt-bot/bot.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const builder = require("botbuilder");
+const dialog = require("./dialogs/luis");
+const bot = new builder.UniversalBot(
+    new builder.ChatConnector({
+        appId: process.env.MICROSOFT_APP_ID,
+        appPassword: process.env.MICROSOFT_APP_PASSWORD
+    }), 
+    dialog.waterfall
+);
+
+bot.recognizer(new builder.LuisRecognizer(process.env.LUIS_MODEL_URL));
+
+module.exports = bot;
